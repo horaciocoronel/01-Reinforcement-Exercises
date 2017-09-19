5a0ed061a7304e45f12a3fbf77f5f2a448e819d4
horaciocoronel <arielc@gmail.com>
2017-09-19 14:39:58 -0400

    Ask questions and answer accordginly

--- exercise1.rb
+++ exercise1.rb
@@ -7,23 +7,27 @@ dramedy = "Lost in Translation"
 # Ask the user for genre input
 puts "Do you enjoy Documentaries?"
 user_documentary = gets.chomp.downcase
-  if user_documentary == "yes" || user_documentary == "y"
-    puts "If you like Documentaries you will enjoy '#{documentary}'. Check it out."
-  elsif user_documentary == "no" || user_documentary == "n"
-  end
-
 puts "Do you enjoy Dramas?"
 user_drama = gets.chomp.downcase
-  if user_drama == "yes" || user_drama == "y"
-    puts "If you like Dramas you will enjoy '#{drama}'. Check it out."
-  elsif user_drama == "no" || user_drama == "n"
-  end
 puts "Do you enjoy Comedies?"
 user_comedy = gets.chomp.downcase
-  if user_comedy == "yes" || user_comedy == "y"
-    puts "If you like Comedies you will enjoy '#{comedy}'. Check it out."
-  elsif user_comedy == "no" || user_comedy == "n"
-  end
-if user_documentary == "no" || user_documentary == "n" && user_drama == "no" || user_drama == "n" || user_comedy == "no" || user_comedy == "n"
+
+if (user_documentary == "no") && (user_drama == "no") && (user_comedy == "no")
     puts "I'll recommend a good book for you. Try reading 'War and Peace' by Leo Tolstoy"
+else
+  # Goes Next
+  if (user_drama == "yes") && (user_comedy == "yes")
+      puts "If you like Dramedies you will enjoy '#{dramedy}'. Check it out."
   end
+
+  if user_documentary == "yes"
+      puts "If you like Documentaries you will enjoy '#{documentary}'. Check it out."
+  end
+
+  if user_drama == "yes" && user_comedy == "no"
+        puts "If you like Dramas you will enjoy '#{drama}'. Check it out."
+  end
+  if user_comedy == "yes" && user_drama == "no"
+        puts "If you like Comedies you will enjoy '#{comedy}'. Check it out."
+  end
+end
