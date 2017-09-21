e7e5d7a370324b7907029a7ae4b1fd5694e3844b
horaciocoronel <arielc@gmail.com>
2017-09-21 14:00:47 -0400

    Fundamentals Reinforcement - Exercise 1

---
+++ programming-fundamentals/exercise1.rb
@@ -0,0 +1,14 @@
+trains_schedule = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+# 1. Save the direction of train 111 into a variable.
+train_111 = trains_schedule.select { |k, v| k[:train] == "111" }
+train_111_direction = train_111[0][:direction]
